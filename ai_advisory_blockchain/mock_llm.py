2f7edabf779c7a36c46e82f85413f30d8ec9fc70
GHBhoreNam <namratabhore@gmail.com>
2026-09-01 23:31:41 +0530

    Remove recommendation text generation
    Removed the call to generate_recommendation_text function.

--- ai_advisory_blockchain/mock_llm.py
+++ ai_advisory_blockchain/mock_llm.py
@@ -35,11 +35,4 @@ def generate_recommendation_text(
 
     return call_llm(prompt)
 
-recommendation_text = generate_recommendation_text(
-    thought["investor_id"],
-    thought["risk_tolerance"].title(),
-    list(thought["allocation"].keys()),
-    observation["portfolio_expected_return"],
-    observation["portfolio_std_dev"],
-)
 
